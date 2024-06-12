ee0a78da0bf334cf4e0cf317bceba3ae0aad9315
inprograming <dlsdudrn20022@gmail.com>
2024-06-12 09:45:54 +0900

    Feat: 알림(notification) 구현 및 ai_report 알림 내용

--- home_protect/lib/components/ai_report.dart
+++ home_protect/lib/components/ai_report.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:home_protect/components/ai_widget.dart';
+import 'package:home_protect/components/notifications.dart';
 import 'package:home_protect/controller/aws_video.dart'; // Import the VideoController
 
 class AiReport extends StatefulWidget {
@@ -12,17 +13,27 @@ class AiReport extends StatefulWidget {
 class _AiReportState extends State<AiReport> {
   final List<Map<String, String>> alarms = [];
   final VideoController videoController = VideoController();
+  final NotificationService notificationService =
+      NotificationService(); // NotificationService 인스턴스 생성
 
   void _addAlarm() async {
     try {
       final videoUrl = await videoController.fetchVideoUrl();
+      final currentTime =
+          DateTime.now().toString().substring(0, 19); // 현재 날짜와 시간
       setState(() {
         alarms.add({
-          'date': DateTime.now().toString().substring(0, 19), // 날짜와 시간 포함
+          'date': currentTime,
           'description': '새로운 이상행동 감지',
           'url': videoUrl,
         });
       });
+
+      // 푸시 알림 표시
+      notificationService.showNotification(
+        '$currentTime - New Dection',
+        '새로운 이상행동이 감지되었습니다.',
+      );
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error fetching video URL: $e')),
---
+++ home_protect/lib/components/notifications.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  NotificationService() {
+    _initializeNotifications();
+  }
+
+  void _initializeNotifications() {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) async {
+        if (response.payload != null) {
+          // Handle notification tapped logic here
+        }
+      },
+    );
+  }
+
+  Future<void> showNotification(String title, String body) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'item x',
+    );
+  }
+}
