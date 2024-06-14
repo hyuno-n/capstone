426bd5619d3c761e996ae071493023ca24d4f3d3
parkdaehyun17 <ztaiga456@naver.com>
2024-06-15 00:05:27 +0900

    delete

--- lib/binding/init_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:home_protect/controller/app_controller.dart';
-
-class InitBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.put(AppController());
-  }
-}
