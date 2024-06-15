e974396be9c3192f2dd0876ffeb5d6294f054de2
parkdaehyun17 <ztaiga456@naver.com>
2024-06-15 18:35:25 +0900

    push

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:io'; // 추가된 부분
-import 'package:path/path.dart' as path;
 import 'my_home_page.dart'; // 유지하는 부분
 
 Future<void> main() async {
   // 실행 디렉토리의 절대 경로를 설정
-
   await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
