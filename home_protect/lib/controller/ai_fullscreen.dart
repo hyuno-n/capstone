ceb52ecf5819c5792c3a1db96e5661e547723fdb
inprograming <dlsdudrn20022@gmail.com>
2024-10-24 22:35:55 +0900

    feat: monitoring page->문제 개선 및 슬라이더 표시, fullscreen->오류 해결

--- home_protect/lib/controller/ai_fullscreen.dart
+++ home_protect/lib/controller/ai_fullscreen.dart
@@ -12,8 +12,6 @@ class FullscreenVideoPage extends StatefulWidget {
 
 class _FullscreenVideoPageState extends State<FullscreenVideoPage> {
   late VlcPlayerController vlcViewController;
-  bool _isPlaying = true;
-  double _sliderValue = 0.0;
 
   @override
   void initState() {
@@ -22,105 +20,29 @@ class _FullscreenVideoPageState extends State<FullscreenVideoPage> {
       widget.rtspUrl,
       hwAcc: HwAcc.full,
       autoPlay: true,
-      options: VlcPlayerOptions(
-        advanced: VlcAdvancedOptions([
-          VlcAdvancedOptions.networkCaching(4000), // 네트워크 캐싱 시간 조정 (3초)
-        ]),
-        rtp: VlcRtpOptions([
-          VlcRtpOptions.rtpOverRtsp(false),
-        ]),
-      ),
+      options: VlcPlayerOptions(),
     );
-    vlcViewController.addListener(_onPlayerStateChanged);
-  }
-
-  void _onPlayerStateChanged() {
-    if (!mounted) return;
-    setState(() {
-      _sliderValue = vlcViewController.value.position.inSeconds.toDouble();
-    });
   }
 
   @override
   void dispose() {
-    vlcViewController.removeListener(_onPlayerStateChanged);
     vlcViewController.dispose();
     super.dispose();
-  }
-
-  void _onPlayPauseButtonPressed() {
-    setState(() {
-      if (_isPlaying) {
-        vlcViewController.pause();
-      } else {
-        vlcViewController.play();
-      }
-      _isPlaying = !_isPlaying;
-    });
-  }
-
-  void _onSliderChanged(double value) {
-    setState(() {
-      _sliderValue = value;
-      vlcViewController.setTime((value * 1000).toInt());
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Center(
-            child: VlcPlayer(
-              controller: vlcViewController,
-              aspectRatio: MediaQuery.of(context).size.aspectRatio,
-              placeholder: const Center(
-                child: CircularProgressIndicator(),
-              ),
-            ),
+      body: Center(
+        child: VlcPlayer(
+          controller: vlcViewController,
+          aspectRatio: MediaQuery.of(context).size.aspectRatio,
+          placeholder: const Center(
+            child: CircularProgressIndicator(),
           ),
-          Positioned(
-            bottom: 20,
-            left: 20,
-            right: 20,
-            child: Column(
-              children: [
-                Slider(
-                  value: _sliderValue,
-                  min: 0.0,
-                  max: (vlcViewController.value.duration.inSeconds).toDouble(),
-                  onChanged: _onSliderChanged,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        _isPlaying ? Icons.pause : Icons.play_arrow,
-                        color: Colors.white,
-                      ),
-                      onPressed: _onPlayPauseButtonPressed,
-                    ),
-                    const SizedBox(width: 20),
-                    Text(
-                      '${_formatDuration(vlcViewController.value.position)} / ${_formatDuration(vlcViewController.value.duration)}',
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
-  }
-
-  String _formatDuration(Duration duration) {
-    final minutes = duration.inMinutes.remainder(60).toString().padLeft(2, '0');
-    final seconds = duration.inSeconds.remainder(60).toString().padLeft(2, '0');
-    return '$minutes:$seconds';
   }
 }
