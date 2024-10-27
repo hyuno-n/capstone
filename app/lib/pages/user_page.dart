8e86839f2c9ddab1795e2c3f8f1c06b464f61c15
inprograming <dlsdudrn20022@gmail.com>
2024-10-28 01:35:40 +0900

    feat:user_page.dart->카매라 개수 추가 및 삭제할 때 마다 바뀌는 기능 추가

--- app/lib/pages/user_page.dart
+++ app/lib/pages/user_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter_svg/svg.dart';
 import 'package:app/components/drawer_widget.dart';
 import 'package:app/components/enddrawer_widget.dart';
 import 'package:app/controller/user_controller.dart';
+import 'package:app/controller/camera_provider.dart'; // CameraProvider import 추가
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
 class User_page extends StatelessWidget {
   const User_page({super.key});
@@ -11,26 +13,28 @@ class User_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final UserController userController = Get.find();
+    final cameraProvider =
+        Provider.of<CameraProvider>(context); // CameraProvider 인스턴스 가져오기
 
     return Scaffold(
-      backgroundColor: Colors.white, // 배경색을 흰색으로 설정
+      backgroundColor: Colors.white,
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(56.0), // AppBar 기본 높이
+        preferredSize: const Size.fromHeight(56.0),
         child: Container(
           decoration: BoxDecoration(
-            color: Colors.white, // AppBar 배경을 흰색으로 설정
+            color: Colors.white,
             boxShadow: [
               BoxShadow(
-                color: Colors.grey.withOpacity(0.3), // 그림자 색상
-                spreadRadius: 0.8, // 그림자의 퍼짐 정도
-                blurRadius: 5, // 그림자 흐림 정도
-                offset: const Offset(0, 2), // 그림자 위치 (아래쪽으로 약간)
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 0.8,
+                blurRadius: 5,
+                offset: const Offset(0, 2),
               ),
             ],
           ),
           child: AppBar(
-            elevation: 0, // 기본 elevation 제거
-            backgroundColor: Colors.transparent, // 투명 배경 설정
+            elevation: 0,
+            backgroundColor: Colors.transparent,
             title: const Text(
               "User page",
               style: TextStyle(fontSize: 15),
@@ -89,14 +93,13 @@ class User_page extends StatelessWidget {
               height: 85,
               decoration: BoxDecoration(
                 color: const Color.fromARGB(255, 255, 255, 255),
-                borderRadius:
-                    const BorderRadius.all(Radius.circular(5)), // 모든 모서리를 둥글게
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.13), // 그림자 색상 및 투명도
-                    spreadRadius: 0.5, // 그림자의 퍼짐 정도
-                    blurRadius: 5, // 그림자의 흐림 정도
-                    offset: const Offset(1, 1), // 그림자의 위치 (세로로 3px 아래로 이동)
+                    color: Colors.black.withOpacity(0.13),
+                    spreadRadius: 0.5,
+                    blurRadius: 5,
+                    offset: const Offset(1, 1),
                   ),
                 ],
               ),
@@ -108,116 +111,95 @@ class User_page extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Image.asset(
-                            'assets/images/user_detection_icon.gif', // GIF 아이콘 경로
-                            width: 40, // 아이콘의 너비
-                            height: 40, // 아이콘의 높이
+                            'assets/images/user_detection_icon.gif',
+                            width: 40,
+                            height: 40,
                           ),
-                          const SizedBox(height: 1), // 아이콘과 텍스트 간격
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                '감지된 건 ', // 개행 없이 수평 배치
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              Text(
-                                '0', // 수평으로 나열
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
+                          const SizedBox(height: 1),
+                          const Text(
+                            '감지된 건 ',
+                            style: TextStyle(fontSize: 14, color: Colors.black),
+                          ),
+                          const Text(
+                            '0',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
                           ),
                         ],
                       ),
                     ),
                   ),
-
                   const SizedBox(
-                    height: 50, // 원하는 높이로 설정
+                    height: 50,
                     child: VerticalDivider(
-                      width: 0.5, thickness: 1, // 구분선의 두께
+                      width: 0.5,
+                      thickness: 1,
                       color: Color.fromARGB(255, 223, 223, 223),
                     ),
-                  ), // 구분선 추가
+                  ),
                   Expanded(
                     child: Center(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Image.asset(
-                            'assets/images/user_camera_icon.gif', // GIF 아이콘 경로
-                            width: 40, // 아이콘의 너비
-                            height: 40, // 아이콘의 높이
+                            'assets/images/user_camera_icon.gif',
+                            width: 40,
+                            height: 40,
                           ),
-                          const SizedBox(height: 1), // 아이콘과 텍스트 간격
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                '카매라 개수 ', // 개행 없이 수평 배치
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              Text(
-                                '2', // 수평으로 나열
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
+                          const SizedBox(height: 1),
+                          const Text(
+                            '카메라 개수', // 카메라 개수 표시
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            '${cameraProvider.rtspUrls.length}', // 카메라 개수 표시
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
                           ),
                         ],
                       ),
                     ),
                   ),
-
                   const SizedBox(
-                    height: 50, // 원하는 높이로 설정
+                    height: 50,
                     child: VerticalDivider(
-                      width: 0.5, thickness: 1, // 구분선의 두께
+                      width: 0.5,
+                      thickness: 1,
                       color: Color.fromARGB(255, 223, 223, 223),
                     ),
-                  ), // 구분선 추가
+                  ),
                   Expanded(
                     child: Center(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Image.asset(
-                            'assets/images/user_clip_icon.gif', // GIF 아이콘 경로
-                            width: 40, // 아이콘의 너비
-                            height: 40, // 아이콘의 높이
+                            'assets/images/user_clip_icon.gif',
+                            width: 40,
+                            height: 40,
                           ),
-                          const SizedBox(height: 1), // 아이콘과 텍스트 간격
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                '클립 개수 ', // 개행 없이 수평 배치
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              Text(
-                                '0', // 수평으로 나열
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
+                          const SizedBox(height: 1),
+                          const Text(
+                            '클립 개수 ',
+                            style: TextStyle(fontSize: 14, color: Colors.black),
+                          ),
+                          const Text(
+                            '0',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
                           ),
                         ],
                       ),
@@ -231,8 +213,8 @@ class User_page extends StatelessWidget {
           const Divider(
             thickness: 1,
             color: Color.fromARGB(255, 218, 214, 214),
-            indent: 20, // 왼쪽 여백
-            endIndent: 20, // 오른쪽 여백
+            indent: 20,
+            endIndent: 20,
           ),
           const SizedBox(height: 10),
           Expanded(
@@ -241,24 +223,20 @@ class User_page extends StatelessWidget {
               children: [
                 ListTile(
                   leading: Image.asset('assets/images/setting_icon.gif',
-                      width: 30, height: 30), // GIF 아이콘 추가
+                      width: 30, height: 30),
                   title: const Text('설정'),
                   trailing: const Icon(Icons.arrow_forward_ios),
-                  onTap: () {
-                    // 설정 클릭 시 동작
-                  },
+                  onTap: () {},
                 ),
-                const SizedBox(height: 10), // 버튼 간격 추가
+                const SizedBox(height: 10),
                 ListTile(
                   leading: Image.asset('assets/images/notice_icon.gif',
-                      width: 30, height: 30), // GIF 아이콘 추가
+                      width: 30, height: 30),
                   title: const Text('공지사항'),
                   trailing: const Icon(Icons.arrow_forward_ios),
-                  onTap: () {
-                    // 공지사항 클릭 시 동작
-                  },
+                  onTap: () {},
                 ),
-                const SizedBox(height: 10), // 버튼 간격 추가
+                const SizedBox(height: 10),
               ],
             ),
           ),
